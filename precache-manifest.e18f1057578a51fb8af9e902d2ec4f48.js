177e6b75c0c368bfb2b342a056d42e84e5b7a0f8
Tal <talvindersingh1992@gmail.com>
2019-09-23 20:30:35 +0530

    Updates

---
+++ precache-manifest.e18f1057578a51fb8af9e902d2ec4f48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee4763a4970723ae11145f2e5ccc7a29",
+    "url": "/memegenerator/index.html"
+  },
+  {
+    "revision": "dc5de54bfb424d6a942c",
+    "url": "/memegenerator/static/css/main.4b280e6a.chunk.css"
+  },
+  {
+    "revision": "888619c68294948251df",
+    "url": "/memegenerator/static/js/2.419caf61.chunk.js"
+  },
+  {
+    "revision": "dc5de54bfb424d6a942c",
+    "url": "/memegenerator/static/js/main.e82fa7ff.chunk.js"
+  },
+  {
+    "revision": "bf242ca7cfa95deb469e",
+    "url": "/memegenerator/static/js/runtime-main.0e91939f.js"
+  }
+]);
